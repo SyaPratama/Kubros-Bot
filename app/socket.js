03c4fc9ad6b652caa7cb24f1102eacdbe2b830ed
RasyaPrtma <rasyaputraq17@gmail.com>
2025-01-02 11:55:59 +0700

    feat: pairing code

--- app/socket.js
+++ app/socket.js
@@ -1,4 +1,7 @@
+import inquirer from "inquirer";
 import { STORAGE_MEMORY } from "../config.js";
+import { Browsers } from "@whiskeysockets/baileys";
+import fs from "fs/promises";
 
 (async () => {
   const {
@@ -6,7 +9,6 @@ import { STORAGE_MEMORY } from "../config.js";
     DisconnectReason,
     useMultiFileAuthState,
     makeCacheableSignalKeyStore,
-    PHONENUMBER_MCC,
     makeInMemoryStore
   } = await import("@whiskeysockets/baileys");
   const NodeCache = await import("node-cache");
@@ -16,6 +18,9 @@ import { STORAGE_MEMORY } from "../config.js";
   const { Log } = await import("../helper/logger.js");
   const event = await import("./event/index.js");
   const readline = await import("readline");
+  let useCode = {
+    isTrue: true
+  }
 
   const store = makeInMemoryStore({ Log });
   store?.readFromFile(path.join(STORAGE_MEMORY,'/kus_store_multi.json'));
@@ -24,23 +29,13 @@ import { STORAGE_MEMORY } from "../config.js";
   },10_000)
 
   const startSocket = async () => {
-    const rl = readline.createInterface({
-      input: process.stdin,
-      output: process.stdout,
-    });
-    const usePairingCode = process.argv.includes("--pairing-code");
-    const question = (text) => {
-      return new Promise((resolve) => {
-        rl.question(text, resolve);
-      });
-    };
     let retryCount = 0;
     const msgRetryCounterCache = new NodeCache.default();
     const { state, saveCreds } = await useMultiFileAuthState(
       path.join(STORAGE_SESSION, SESSION_NAME)
     );
     const sock = makeWASocket({
-      printQRInTerminal: !usePairingCode,
+      printQRInTerminal: false,
       auth: {
         creds: state.creds,
         keys: makeCacheableSignalKeyStore(
@@ -52,45 +47,43 @@ import { STORAGE_MEMORY } from "../config.js";
         ),
       },
       logger: pino.default({ level: "silent" }),
-      browser: ["Chrome (Linux)", "", ""],
+      browser: Browsers.ubuntu("Chrome"),
       markOnlineOnConnect: true,
       generateHighQualityLinkPreview: true,
       msgRetryCounterCache,
       defaultQueryTimeoutMs: undefined,
     });
+    if (useCode.isTrue && !sock.authState.creds.registered) {
+        useCode = await inquirer.prompt([
+          {
+            type:"confirm",
+            name:"isTrue",
+            message:"Apakah Anda Ingin Menggunakan Pairing Code?",
+            default: true,
+          }
+        ]);
+        if(useCode.isTrue)
+        {
+          const numWa = await inquirer.prompt([{
+            type:'input',
+            name:'res',
+            message: 'Nomor Whatsapp :',
+          }]);
+          
+          const code = await sock.requestPairingCode(numWa.res);
+          console.log(code);
+        } else {
+          useCode.isTrue = false;
+          startSocket();
+        }
+    }
 
     store?.bind(sock.ev);
-    if (usePairingCode && !sock.authState.creds.registered) {
-      let phoneNumber = await question(
-        "Masukan Nomor Hp Mu Contoh +628124534434 : \n"
-      );
-      phoneNumber = phoneNumber.replace(/[^0-9]/g, "");
-      if (
-        !Object.keys(PHONENUMBER_MCC).some((value) =>
-          phoneNumber.startsWith(value)
-        )
-      ) {
-        Log.info(
-          "Nomor Whatsapp Dimulai Dengan Code Negara, Contoh : +628124534434"
-        );
-        phoneNumber = await question(
-          "Masukan Nomor Hp Mu Contoh +628124534434 : \n"
-        );
-        phoneNumber = phoneNumber.replace(/[^0-9]/g, "");
-        rl.close();
-      }
-
-      setTimeout(async () => {
-        let code = await sock.requestPairingCode(phoneNumber);
-        code = code?.match(/.{1,4}/g)?.join("-") || code;
-        Log.info(`Your Pairing Code: ${code}`);
-      }, 3000);
-    }
 
     await event.default(sock);
 
     try {
-      sock.ev.on("connection.update", (update) => {
+      sock.ev.on("connection.update", async (update) => {
         const { connection, lastDisconnect } = update;
         if (connection == "connecting") {
           Log.debug(`SESSION : Conecting.`);
@@ -112,6 +105,7 @@ import { STORAGE_MEMORY } from "../config.js";
             retryCount = retryAttempt;
             startSocket();
           } else {
+            await fs.rm(path.join(STORAGE_SESSION,SESSION_NAME));
             Log.error(`SESSION : Disconnected.`);
             retryCount = 0;
             sock?.logout();
